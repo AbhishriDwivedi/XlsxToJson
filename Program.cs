32e293d3889277a43bea6dbe45068840fe3d8035
AbhishriDwivedi <40824781+AbhishriDwivedi@users.noreply.github.com>
2022-10-17 12:04:48 +0530

    closing the excel

--- Program.cs
+++ Program.cs
@@ -1,23 +1,27 @@
 ﻿
-using System;
-using System.Data.Common;
-using System.Data.OleDb;
-using System.Text;
-using ExcelDataReader;
+using System.Runtime.InteropServices;
 using Microsoft.Office.Interop.Excel;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace nms
 {
     class read_from_excel_file
     {
+        public class Root
+        {
+            public string Name { get; set; }
+            public double Age { get; set; }
+            public string Gender { get; set; }
+            public string Profession { get; set; }
+            public string Attendance { get; set; }
+        }
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Please enter the path for xlsx file to be converted:");
             string inputPath = Console.ReadLine();
             if (!File.Exists(inputPath)) { Console.WriteLine("Exiting program due to wrong input"); return; }
-            string strFileName = inputPath; //C:\Users\ja0136\Desktop\Attendance.xlsx
+            string strFileName = inputPath; //D:\People.xlsx
             if (!File.Exists(strFileName))
             {
                 Console.WriteLine("Invalid Location");
@@ -29,46 +33,53 @@ namespace nms
             string name = Console.ReadLine();
             string destFileName = Path.Combine(destination, name);
             Application excel = new Application();
-            Workbook workBook = excel.Application.Workbooks.Open(strFileName);
-            Worksheet worksheet = (Worksheet)workBook.Sheets[1];
-            int columns = 1;
-            while (worksheet.Cells[1, columns].Value != null)
-            {
-                columns++;
-            }
-            columns -= 1;
-            int rows = 1;
-            while (worksheet.Cells[rows, 1].Value != null)
-            {
-                rows++;
-            }
-            rows -= 1;
-           JArray array= new JArray();
-            JObject o = new JObject();
+            Workbooks workBooks = excel.Workbooks;
+            Workbook workBook = workBooks.Open(strFileName);
+            Sheets sheets = workBook.Sheets;
+            Worksheet worksheet = sheets[1];
+
+            var UR = worksheet.UsedRange;
+
+            var usedRange = UR.Value;
+            int rows = usedRange.Length/5;
+            var obj = new Root();
+            List<Root> list = new List<Root>();
             for (int i = 2; i <= rows; i++)
             {
-                o = new JObject();
-                for (int j = 1; j <= columns; j++)
-                {   
-                    o.Add((worksheet.Cells[1, j].Value).ToString(), worksheet.Cells[i, j].Value);
-                }
-                array.Add(o);
+                obj = new Root()
+                {
+                    Name = usedRange[i, 1],
+                    Age = usedRange[i, 2],
+                    Gender = usedRange[i, 3],
+                    Profession = usedRange[i, 4],
+                    Attendance = usedRange[i, 5]
+                };
+                list.Add(obj);
             }
 
-            string json = o.ToString();
-            Console.WriteLine(array);
             try
             {
-                var details = JsonConvert.SerializeObject(array);
+                string details = JsonConvert.SerializeObject(list);
+                Console.Write(details);
                 File.WriteAllText(destFileName, details.ToString());
                 Console.WriteLine("File is created in the requested location");
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
+            Marshal.FinalReleaseComObject(UR);
+            Marshal.FinalReleaseComObject(worksheet);
+            Marshal.FinalReleaseComObject(sheets);
+            workBook.Close();
+            Marshal.FinalReleaseComObject(workBook);
+            workBooks.Close();
+            Marshal.FinalReleaseComObject(workBooks);
+            excel.Quit();
+            Marshal.FinalReleaseComObject(excel);
         }
-
-       
     }
-}
+    }
+
+
